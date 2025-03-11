446d87e2ac89305156949f3f4a0372280831328f
Acohen455 <aaroncohen455@gmail.com>
2025-03-11 12:35:13 -0700

    Updates

--- BookAPI/src/main/java/com/revature/BookAPI/controllers/BookController.java
+++ BookAPI/src/main/java/com/revature/BookAPI/controllers/BookController.java
@@ -1,16 +1,31 @@
 package com.revature.BookAPI.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController // combo of @Controller and @ResponseBody -- second turns things into json
 @RequestMapping("/books")
 @CrossOrigin
 public class BookController {
+
+
+    /*
+    private final RestTemplate restTemplate;
+
+    @Autowired
+    public BookController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+
+
+    }
+     */
+
 
     //Method that doesn't interact with any other API
     @GetMapping
@@ -28,8 +43,14 @@ public class BookController {
     @GetMapping("/recs")
     public ResponseEntity<String[]> getBookRecs() {
 
-        //using RestTemplate to send an HTTP Request to another API
+        //Instantiating a resttemplate so we can make http requests
+        RestTemplate restTemplate =  new RestTemplate();
 
+        //using RestTemplate to send an HTTP Request to another API
+        //the String[].class passes it the blueprint instead of the object -- ie. passing the class instead of the object
+        String[] recs = restTemplate.getForObject("http://localhost:8081/recs", String[].class);
+
+        return ResponseEntity.ok(recs);
 
     }
 
